5e95644f77eaa22b77f2092deb717d939040899f
Ucrash <gducrash@gmail.com>
2023-02-24 16:25:44 +0200

    fix .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginRadar({
-      analytics: {
-        id: import.meta.VITE_GANALYTICS_KEY
-      }
-    })
-  ],
-});
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [
+      react(),
+      VitePluginRadar({
+        analytics: {
+          id: process.env.VITE_GANALYTICS_KEY
+        }
+      })
+    ],
+  });
+}
